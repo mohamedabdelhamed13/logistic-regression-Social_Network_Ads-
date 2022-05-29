fc5fbdb2246455e1f36eec38eae05e47b16c15b9
Muhammed Abdelhamid <67540846+mohamedabdelhamed13@users.noreply.github.com>
2022-05-29 18:58:27 +0200

    Add files via upload

---
+++ logistic regression task.py
@@ -0,0 +1,125 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[24]:
+
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+# In[25]:
+
+
+dataset = pd.read_csv('C:\\Users\\moham\\OneDrive\\Desktop\\projects\\logistic regression task\\Social_Network_Ads.csv')
+
+
+# In[26]:
+
+
+dataset.head(10)
+
+
+# In[27]:
+
+
+dataset.describe()
+
+
+# In[28]:
+
+
+# input
+x = dataset.iloc[:, [2, 3]].values
+
+# output
+y = dataset.iloc[:, 4].values
+
+
+# In[ ]:
+
+
+
+
+
+# In[29]:
+
+#train 75% of data 
+from sklearn.model_selection import train_test_split
+xtrain, xtest, ytrain, ytest = train_test_split(x, y, test_size = 0.25, random_state = 0)
+
+
+# In[30]:
+
+# Z score
+from sklearn.preprocessing import StandardScaler
+sc_x = StandardScaler()
+xtrain = sc_x.fit_transform(xtrain)
+xtest = sc_x.transform(xtest)
+
+print (xtrain[0:10, :])
+
+
+# In[31]:
+
+#training model by logistic regression
+from sklearn.linear_model import LogisticRegression
+classifier = LogisticRegression(random_state = 0)
+classifier.fit(xtrain, ytrain)
+
+
+# In[32]:
+
+# make prediction
+
+y_pred = classifier.predict(xtest)
+
+
+# In[33]:
+
+#test the performance by confusion matrix 65+24 true positive , negative =89
+# 3 +8 false negative , false negative 
+
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(ytest, y_pred)
+
+print ("Confusion Matrix : \n", cm)
+
+
+# In[34]:
+
+
+from sklearn.metrics import accuracy_score
+print ("Accuracy : ", accuracy_score(ytest, y_pred))
+
+
+# In[35]:
+
+
+from matplotlib.colors import ListedColormap
+X_set, y_set = xtest, ytest
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1,stop = X_set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = X_set[:, 1].min() - 1,stop = X_set[:, 1].max() + 1, step = 0.01))
+
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape), alpha = 0.75,
+             cmap = ListedColormap(('red', 'blue')))
+
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+# the points
+for i, j in enumerate(np.unique(y_set)):plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                                                    c = ListedColormap(('green', 'black'))(i), label = j)
+
+plt.title('Classifier (Test set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
+
+
+# In[ ]:
+
+
+
+
